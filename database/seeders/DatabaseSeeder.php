2896480d267e1674f19203d426a930fb08be2671
aapeengggg <mirfanmanaf0804@gmail.com>
2024-10-06 06:10:57 +0700

    seeder mobil

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             MUsersSeeder::class,
             MUserDatasSeeder::class,
             MMerekMobilsSeeder::class,
-            // MMobilsSeeder::class,
+            MMobilsSeeder::class,
             MFasilitasSeeder::class,
         ]);
     }
