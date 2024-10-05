3f588b7094ea8a2552d77ad866942decfb579765
aapeengggg <mirfanmanaf0804@gmail.com>
2024-10-06 04:45:58 +0700

    fix footer

--- resources/js/layouts/components/Footer.vue
+++ resources/js/layouts/components/Footer.vue
@@ -16,22 +16,7 @@
         target="_blank"
         rel="noopener noreferrer"
         class="text-primary ms-1"
-      >Pixinvent</a>
-    </span>
-    <!-- 👉 Footer: right content -->
-    <span class="d-md-flex gap-x-4 text-primary d-none">
-      <a
-        href="https://themeforest.net/licenses/standard"
-        target="noopener noreferrer"
-      >License</a>
-      <a
-        href="https://pixinvent.com/"
-        target="noopener noreferrer"
-      >More Themes</a>
-      <a
-        href="https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/documentation/"
-        target="noopener noreferrer"
-      >Documentation</a>
+      >Mohamad Irfan Manaf</a>
     </span>
   </div>
 </template>
